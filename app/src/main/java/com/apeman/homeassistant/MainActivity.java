b6eed077dff49d2b26c0e5d63783dfd76ae3b279
Sebastian Matuszczyk <matuszczyksebastian9@gmail.com>
2022-03-15 00:40:22 +0100

    API calls on refresh button click

--- app/src/main/java/com/apeman/homeassistant/MainActivity.java
+++ app/src/main/java/com/apeman/homeassistant/MainActivity.java
@@ -4,11 +4,74 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView temp = (TextView) findViewById(R.id.temperature);
+        TextView humi = (TextView) findViewById(R.id.humidity);
+
+        Button refresh = (Button) findViewById(R.id.button);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendRequest(temp, humi);
+            }
+        });
+
+    }
+
+    private void sendRequest(TextView temp, TextView humi) {
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
+        Network network = new BasicNetwork(new HurlStack());
+        RequestQueue requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
+
+        String url = getResources().getString(R.string.get_temperature_URL);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            temp.setText(jsonObject.getString("V0"));
+                            humi.setText(jsonObject.getString("V1"));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        temp.setText("404");
+                    }
+                }
+        );
+
+        requestQueue.add(stringRequest);
     }
 }
