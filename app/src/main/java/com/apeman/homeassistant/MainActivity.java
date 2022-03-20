1b322ff3247c98a1c2f81d47f1abee7f3e7082ed
Sebastian Matuszczyk <matuszczyksebastian9@gmail.com>
2022-03-20 22:56:12 +0100

    implemented working GridView with cards, but there is a problem with data which has to be resolved

--- app/src/main/java/com/apeman/homeassistant/MainActivity.java
+++ app/src/main/java/com/apeman/homeassistant/MainActivity.java
@@ -26,29 +26,51 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.sql.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-    String[] values = new String[2];
+    HashMap<String, String> temperatureValues = new HashMap<>();
+    GridView gridView;
+
     private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        gridView = findViewById(R.id.cardsDeck);
+        sendRequest();
+
+        Log.i(TAG, "temperatura: " + temperatureValues.get("insideTemperature"));
+        Log.i(TAG, "wilgotność: " + temperatureValues.get("insideHumidity"));
+        Log.i(TAG, "klucze: " + temperatureValues.keySet());
+        Log.i(TAG, "wartosci: " + temperatureValues.values());
+
+        ArrayList<CardContent> cardContentArrayList = new ArrayList<CardContent>();
+        cardContentArrayList.add(new CardContent(
+                temperatureValues.get("insideTemperature"),
+                "Czujnik temperatury wew."
+        ));
+        cardContentArrayList.add(new CardContent(
+                temperatureValues.get("insideHumidity"),
+                "Wilgotność"
+        ));
+
+        GridAdapter adapter = new GridAdapter(this, cardContentArrayList);
+        gridView.setAdapter(adapter);
+
 
         ActionMenuItemView refresh = findViewById(R.id.refresh);
-
         refresh.setOnClickListener(view -> {
             // it rotates only once, fix it
             refresh.animate().rotation(360.0f).setDuration(200).start();
-
-            sendRequest();
         });
-
     }
 
-    public void sendRequest() {
+    public synchronized void sendRequest() {
         Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
         Network network = new BasicNetwork(new HurlStack());
         RequestQueue requestQueue = new RequestQueue(cache, network);
@@ -62,8 +84,15 @@ public class MainActivity extends AppCompatActivity {
                         JSONObject jsonObject = new JSONObject(response);
 
                         // Assigning those values by using loop would cause problems in the future
-                        values[0] = jsonObject.getString("V0");
-                        values[1] = jsonObject.getString("V1");
+                        temperatureValues.put("insideTemperature", jsonObject.getString("V0"));
+                        Log.d(TAG, "V0: " + jsonObject.getString("V0"));
+                        temperatureValues.put("insideHumidity", jsonObject.getString("V1"));
+                        Log.d(TAG, "V1: " + jsonObject.getString("V1"));
+
+                        Log.i(TAG, "temperatura: " + temperatureValues.get("insideTemperature"));
+                        Log.i(TAG, "wilgotność: " + temperatureValues.get("insideHumidity"));
+                        Log.i(TAG, "klucze: " + temperatureValues.keySet());
+                        Log.i(TAG, "wartosci: " + temperatureValues.values());
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
