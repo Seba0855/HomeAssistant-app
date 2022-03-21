46b6f2e7fa21af42fba97a2f7ed98e31d0e38ed1
Sebastian Matuszczyk <matuszczyksebastian9@gmail.com>
2022-03-21 15:36:27 +0100

    Changed GridView to RecyclerView with usage of GridLayoutManager in order to increase app performance in the future

--- app/src/main/java/com/apeman/homeassistant/MainActivity.java
+++ app/src/main/java/com/apeman/homeassistant/MainActivity.java
@@ -2,38 +2,36 @@ package com.apeman.homeassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.menu.ActionMenuItemView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import android.util.Log;
 import android.view.Gravity;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.TextView;
 
 import com.android.volley.Cache;
 import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
-import com.google.android.material.appbar.MaterialToolbar;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     HashMap<String, String> temperatureValues = new HashMap<>();
-    GridView gridView;
+    private RecyclerView recyclerView;
+    private ArrayList<CardContent> cardContentArrayList;
 
     private static final String TAG = "MainActivity";
 
@@ -41,18 +39,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        gridView = findViewById(R.id.cardsDeck);
-        gridView.setGravity(Gravity.CENTER);
-        sendRequest();
+        recyclerView = findViewById(R.id.cardsDeck);
+
+        RequestWizard requestWizard = new RequestWizard(this, temperatureValues);
+        requestWizard.sendRequest();
 
         Log.i(TAG, "temperatura: " + temperatureValues.get("insideTemperature"));
         Log.i(TAG, "wilgotność: " + temperatureValues.get("insideHumidity"));
         Log.i(TAG, "klucze: " + temperatureValues.keySet());
         Log.i(TAG, "wartosci: " + temperatureValues.values());
 
-        ArrayList<CardContent> cardContentArrayList = new ArrayList<CardContent>();
+        // Create new ArrayList to store card objects
+        cardContentArrayList = new ArrayList<>();
+
         cardContentArrayList.add(new CardContent(
                 temperatureValues.get("insideTemperature"),
                 "Czujnik temperatury wew."
@@ -62,18 +62,35 @@ public class MainActivity extends AppCompatActivity {
                 "Wilgotność"
         ));
 
-        GridAdapter adapter = new GridAdapter(this, cardContentArrayList);
-        gridView.setAdapter(adapter);
+        RecyclerGridAdapter adapter = new RecyclerGridAdapter(cardContentArrayList, this);
+
+        // Setting grid layout manager to implement grid view, displaying 2 columns
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+        Log.d(TAG, "Utworzył się view");
+        //recyclerView.setForegroundGravity(Gravity.CENTER);
 
 
         ActionMenuItemView refresh = findViewById(R.id.refresh);
         refresh.setOnClickListener(view -> {
             // it rotates only once, fix it
             refresh.animate().rotation(360.0f).setDuration(200).start();
+            requestWizard.sendRequest();
+            cardContentArrayList.get(0).setValue(temperatureValues.get("insideTemperature"));
+            cardContentArrayList.get(1).setValue(temperatureValues.get("insideHumidity"));
+            Log.i(TAG, "temperatura: " + temperatureValues.get("insideTemperature"));
+            Log.i(TAG, "wilgotność: " + temperatureValues.get("insideHumidity"));
+            Log.i(TAG, "klucze: " + temperatureValues.keySet());
+            Log.i(TAG, "wartosci: " + temperatureValues.values());
+            Log.i(TAG, "temperatura z arraya: " + cardContentArrayList.get(0).getValue());
+            adapter.notifyItemChanged(0);
+            adapter.notifyItemChanged(1);
         });
     }
 
-    public synchronized void sendRequest() {
+    public void sendRequest() {
         Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
         Network network = new BasicNetwork(new HurlStack());
         RequestQueue requestQueue = new RequestQueue(cache, network);
@@ -81,28 +98,32 @@ public class MainActivity extends AppCompatActivity {
 
         String uri = getResources().getString(R.string.get_temperature_URL);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, uri,
-                response -> { // onResponse
-                    try {
-                        JSONObject jsonObject = new JSONObject(response);
 
-                        // Assigning those values by using loop would cause problems in the future
-                        temperatureValues.put("insideTemperature", jsonObject.getString("V0"));
-                        Log.d(TAG, "V0: " + jsonObject.getString("V0"));
-                        temperatureValues.put("insideHumidity", jsonObject.getString("V1"));
-                        Log.d(TAG, "V1: " + jsonObject.getString("V1"));
+        Log.e(TAG, "PRÓBA UTWORZENIA STRING REQUESTA");
 
-                        Log.i(TAG, "temperatura: " + temperatureValues.get("insideTemperature"));
-                        Log.i(TAG, "wilgotność: " + temperatureValues.get("insideHumidity"));
-                        Log.i(TAG, "klucze: " + temperatureValues.keySet());
-                        Log.i(TAG, "wartosci: " + temperatureValues.values());
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                },
-                Throwable::printStackTrace // onErrorResponse, use Log.e later
-        );
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, uri, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
 
+                    // Assigning those values by using loop would cause problems in the future
+                    temperatureValues.put("insideTemperature", jsonObject.getString("V0"));
+                    Log.d(TAG, "V0: " + jsonObject.getString("V0"));
+                    temperatureValues.put("insideHumidity", jsonObject.getString("V1"));
+                    Log.d(TAG, "V1: " + jsonObject.getString("V1"));
+
+                } catch (JSONException e) {
+                    Log.e(TAG, "chuij nie wyszło");
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "wystąpił błąd: " + error);
+            }
+        });
         requestQueue.add(stringRequest);
     }
 }
---
+++ app/src/main/java/com/apeman/homeassistant/RecyclerGridAdapter.java
@@ -0,0 +1,77 @@
+package com.apeman.homeassistant;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerGridAdapter extends RecyclerView.Adapter<RecyclerGridAdapter.RecyclerViewHolder> {
+    private ArrayList<CardContent> cardContentArrayList;
+    private Context context;
+
+    public RecyclerGridAdapter(ArrayList<CardContent> recyclerCardContent, Context context) {
+        this.cardContentArrayList = recyclerCardContent;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Layout inflater
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_item, parent, false);
+        return new RecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
+        // Setting the data to value indicator and description
+        CardContent cardContent = cardContentArrayList.get(position);
+        holder.valueIndicator.setText(cardContent.getValue());
+        holder.description.setText(cardContent.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return cardContentArrayList.size();
+    }
+
+    //    @NonNull
+//    @Override
+//    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        View listitemView = convertView;
+//        if (listitemView == null) {
+//            // Layout Inflater inflates each item to be displayed in Grid View
+//            listitemView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
+//        }
+//
+//        CardContent cardContent = getItem(position);
+//        TextView valueIndicator = listitemView.findViewById(R.id.value_indicator);
+//        TextView description = listitemView.findViewById(R.id.name);
+//
+//        valueIndicator.setText(cardContent.getValue());
+//        description.setText(cardContent.getDescription());
+//
+//        return listitemView;
+//    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder{
+        private TextView valueIndicator;
+        private TextView description;
+
+        RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            valueIndicator = itemView.findViewById(R.id.value_indicator);
+            description = itemView.findViewById(R.id.name);
+        }
+    }
+}
+
+
